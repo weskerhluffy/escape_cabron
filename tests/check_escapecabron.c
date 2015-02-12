f89136a52a72cf6c59eb02714cd8acdd6d740791
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2015-02-12 04:56:36 -0600

    se añade prueba de caso de escape viable

--- tests/check_escapecabron.c
+++ tests/check_escapecabron.c
@@ -34,7 +34,44 @@ START_TEST( test_determina_nodos_viables)
 				resultado_real, 1);
 
 		resultado = (nodos_viables_calculados == 2)
-				&& !memcmp(RESULTADO_ESPERADO, resultado_real, 4*(sizeof(tipo_dato)));
+				&& !memcmp(RESULTADO_ESPERADO, resultado_real,
+						4 * (sizeof(tipo_dato)));
+
+		ck_assert_msg(resultado, "verga, la matrix es %d", resultado_real);
+	}END_TEST
+
+START_TEST( test_determina_nodos_viables_caso_posible)
+	{
+		const tipo_dato POSICION_INICIAL_BANDIDOS_DE_AMORES = 2;
+		const tipo_dato POSICION_INICIAL_POLICIAS = 3;
+		const tipo_dato VERTICES[2][3] = { { 1, 2, 7 }, { 2, 3, 8 } };
+		const tipo_dato RESULTADO_ESPERADO[] = { 1, 1, 2, 2 };
+
+		bool resultado = falso;
+		int nodos_viables_calculados = 0;
+		tipo_dato resultado_real[4] = { 0 };
+
+		grafo_contexto grafo_viable_ctx;
+
+		caca_log_debug("me corto las pelotas");
+
+		/*
+		 while (mierda) {
+		 caca_log_debug("FFFFUUUUCCKKK");
+		 }
+		 */
+					sleep(10);
+		//		mierda=0;
+		nodos_viables_calculados = escape_cabron_determina_nodos_viables(
+				(void *) VERTICES, 2, &grafo_viable_ctx,
+				POSICION_INICIAL_POLICIAS, POSICION_INICIAL_BANDIDOS_DE_AMORES);
+
+		grafo_get_representacion_en_matriz_ordenada(&grafo_viable_ctx,
+				resultado_real, 1);
+
+		resultado = (nodos_viables_calculados == 2)
+				&& !memcmp(RESULTADO_ESPERADO, resultado_real,
+						4 * (sizeof(tipo_dato)));
 
 		ck_assert_msg(resultado, "verga, la matrix es %d", resultado_real);
 	}END_TEST
@@ -45,7 +82,8 @@ escapecabron_suite(void) {
 
 	/* Core test case */
 	TCase *tc_core = tcase_create("Core");
-	tcase_add_test(tc_core, test_determina_nodos_viables);
+//	tcase_add_test(tc_core, test_determina_nodos_viables);
+	tcase_add_test(tc_core, test_determina_nodos_viables_caso_posible);
 	suite_add_tcase(s, tc_core);
 
 	return s;
